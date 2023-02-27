db2600810cb6d69043b5f5a3cdacecd3c11b934f
Nok-Nok <myngoc311@gmail.com>
2023-02-27 02:06:26 -0500

    Complete ToDo Practice for React Hook

---
+++ React Hooks/React-Hooks/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+import Todo from './components/Todo.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TO DO Practice</h1>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ React Hooks/React-Hooks/src/components/Todo.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+
+/*
+  INSTRUCTIONS:
+  Create a "todo" app with the following criteria.
+    1. The user can add new todo items
+    2. The user can remove todo items
+*/
+
+function Todo() {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
+  const addTask = () => {
+    setTasks((tasks) => tasks.concat(input));
+    setInput('');
+  };
+  const deleteTask = (i) => {
+    setTasks(tasks.filter((_, idx) => idx !== i));
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="New Todo"
+      />
+      <button onClick={addTask}>Add</button>
+      <ul>
+        {tasks.map((task, i) => {
+          return (
+            <li key={i}>
+              {task}
+              <button onClick={() => deleteTask(i)}>X</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Todo;
