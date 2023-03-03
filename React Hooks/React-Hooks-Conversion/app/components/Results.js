0a879e5b705245bb1be4da87c565c26b9429443b
Nok-Nok <myngoc311@gmail.com>
2023-03-03 15:40:43 -0500

    Refactor Results to use useReducer to have more declarative codes

--- React Hooks/React-Hooks-Conversion/app/components/Results.js
+++ React Hooks/React-Hooks-Conversion/app/components/Results.js
@@ -1,24 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import { battle } from '../utils/api'
-import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa'
-import Card from './Card'
-import PropTypes from 'prop-types'
-import Loading from './Loading'
-import Tooltip from './Tooltip'
-import queryString from 'query-string'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useReducer, useState } from 'react';
+import { battle } from '../utils/api';
+import {
+  FaCompass,
+  FaBriefcase,
+  FaUsers,
+  FaUserFriends,
+  FaCode,
+  FaUser,
+} from 'react-icons/fa';
+import Card from './Card';
+import PropTypes from 'prop-types';
+import Loading from './Loading';
+import Tooltip from './Tooltip';
+import queryString from 'query-string';
+import { Link } from 'react-router-dom';
 
-function ProfileList ({ profile }) {
+function ProfileList({ profile }) {
   return (
-    <ul className='card-list'>
+    <ul className="card-list">
       <li>
-        <FaUser color='rgb(239, 115, 115)' size={22} />
+        <FaUser color="rgb(239, 115, 115)" size={22} />
         {profile.name}
       </li>
       {profile.location && (
         <li>
           <Tooltip text="User's location">
-            <FaCompass color='rgb(144, 115, 255)' size={22} />
+            <FaCompass color="rgb(144, 115, 255)" size={22} />
             {profile.location}
           </Tooltip>
         </li>
@@ -26,87 +33,105 @@ function ProfileList ({ profile }) {
       {profile.company && (
         <li>
           <Tooltip text="User's company">
-            <FaBriefcase color='#795548' size={22} />
+            <FaBriefcase color="#795548" size={22} />
             {profile.company}
           </Tooltip>
         </li>
       )}
       <li>
-        <FaUsers color='rgb(129, 195, 245)' size={22} />
+        <FaUsers color="rgb(129, 195, 245)" size={22} />
         {profile.followers.toLocaleString()} followers
       </li>
       <li>
-        <FaUserFriends color='rgb(64, 183, 95)' size={22} />
+        <FaUserFriends color="rgb(64, 183, 95)" size={22} />
         {profile.following.toLocaleString()} following
       </li>
     </ul>
-  )
+  );
 }
 
 ProfileList.propTypes = {
   profile: PropTypes.object.isRequired,
-}
+};
 
-export default function Results ({location}) {
-  const [winner, setWinner] = useState(null)
-  const [loser, setLoser] = useState(null)
-  const [error, setError] = useState(null)
-  const [loading, setLoading] = useState(true)
-  useEffect(()=>{
-    setLoading(true)
-    const { playerOne, playerTwo } = queryString.parse(location.search)
-    battle([ playerOne, playerTwo ])
-      .then((players) => {
-        setWinner(players[0])
-        setLoser(players[1])
-        setError(null)
-        setLoading(false)
-      }).catch(({ message }) => {
-        setError(message);
-        setLoading(false)
-      })
-  },[])
+const initialBattleState = {
+  loading: true,
+  error: null,
+  winner: null,
+  loser: null,
+};
 
-  
+const battleReducer = (state, action) => {
+  switch (action.type) {
+    case 'loading':
+      return {
+        ...state,
+        loading: true,
+      };
+    case 'success':
+      return {
+        ...state,
+        loading: false,
+        error: null,
+        winner: action.players[0],
+        loser: action.players[1],
+      };
+    case 'error':
+      return {
+        ...state,
+        loading: false,
+        error: action.error.message,
+      };
+    default:
+      throw new Error('Action type is not supported by battleReducer');
+  }
+};
 
-    if (loading === true) {
-      return <Loading text='Battling' />
-    }
+export default function Results({ location }) {
+  const [{ loading, error, winner, loser }, dispatch] = useReducer(
+    battleReducer,
+    initialBattleState
+  );
+  useEffect(() => {
+    dispatch({ type: 'loading' });
+    const { playerOne, playerTwo } = queryString.parse(location.search);
+    battle([playerOne, playerTwo])
+      .then((players) => dispatch({ type: 'success', players }))
+      .catch(({ message }) => dispatch({ type: 'error', message }));
+  }, [location]);
 
-    if (error) {
-      return (
-        <p className='center-text error'>{error}</p>
-      )
-    }
+  if (loading) {
+    return <Loading text="Battling" />;
+  }
 
-    return (
-      <React.Fragment>
-        <div className='grid space-around container-sm'>
-          <Card
-            header={winner.score === loser.score ? 'Tie' : 'Winner'}
-            subheader={`Score: ${winner.score.toLocaleString()}`}
-            avatar={winner.profile.avatar_url}
-            href={winner.profile.html_url}
-            name={winner.profile.login}
-          >
-            <ProfileList profile={winner.profile}/>
-          </Card>
-          <Card
-            header={winner.score === loser.score ? 'Tie' : 'Loser'}
-            subheader={`Score: ${loser.score.toLocaleString()}`}
-            avatar={loser.profile.avatar_url}
-            name={loser.profile.login}
-            href={loser.profile.html_url}
-          >
-            <ProfileList profile={loser.profile}/>
-          </Card>
-        </div>
-        <Link
-          to='/battle'
-          className='btn dark-btn btn-space'>
-            Reset
-        </Link>
-      </React.Fragment>
-    )
-  
+  if (error) {
+    return <p className="center-text error">{error}</p>;
+  }
+  return (
+    <React.Fragment>
+      <div className="grid space-around container-sm">
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Winner'}
+          subheader={`Score: ${winner.score.toLocaleString()}`}
+          avatar={winner.profile.avatar_url}
+          href={winner.profile.html_url}
+          name={winner.profile.login}
+        >
+          <ProfileList profile={winner.profile} />
+        </Card>
+        <Card
+          header={winner.score === loser.score ? 'Tie' : 'Loser'}
+          subheader={`Score: ${loser.score.toLocaleString()}`}
+          avatar={loser.profile.avatar_url}
+          name={loser.profile.login}
+          href={loser.profile.html_url}
+        >
+          <ProfileList profile={loser.profile} />
+        </Card>
+      </div>
+      <Link to="/battle" className="btn dark-btn btn-space">
+        Reset
+      </Link>
+    </React.Fragment>
+  );
 }
