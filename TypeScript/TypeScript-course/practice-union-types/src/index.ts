d81a8567b70ea3e2a69722f565718aa160715cf5
Nok-Nok <myngoc311@gmail.com>
2023-03-04 12:58:05 -0500

    Complete UnionTypes

---
+++ TypeScript/TypeScript-course/practice-union-types/src/index.ts
@@ -0,0 +1,7 @@
+const app = document.getElementById("app");
+if (app) {
+  app.innerHTML = `
+  <h1>Union Types</h1>
+  <p>We'll practice Union types by writing a few. Check out the prompts in the <code>/src/unionTypes/ts</code> file.
+  `;
+}
