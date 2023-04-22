4cac8be9fb085d1d16673bfc65e3da9b331f6205
Nok-Nok <myngoc311@gmail.com>
2023-04-22 16:21:26 -0400

    Reorganize files

--- Algorithm/Binary Search/searchRange.js
+++ Algorithm/Binary Search/searchRange.js
@@ -104,9 +104,9 @@ function specialBinarySearch(nums, target, goLeft) {
   return idx;
 }
 
-(nums = [5, 7, 7, 8, 8, 10]), (target = 6);
-console.log(searchRange(nums, target));
-(nums = []), (target = 0);
-console.log(searchRange(nums, target));
-(nums = [5, 7, 7, 8, 8, 8, 10]), (target = 8);
-console.log(searchRange(nums, target));
+// (nums = [5, 7, 7, 8, 8, 10]), (target = 6);
+// console.log(searchRange(nums, target));
+// (nums = []), (target = 0);
+// console.log(searchRange(nums, target));
+// (nums = [5, 7, 7, 8, 8, 8, 10]), (target = 8);
+// console.log(searchRange(nums, target));
