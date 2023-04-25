2ac36b739983cd269251beb1b010924e95922a16
Nok-Nok <myngoc311@gmail.com>
2023-04-25 11:39:22 -0400

    Compelte isBalanced

--- Algorithm/Tree/isBalanced.js
+++ Algorithm/Tree/isBalanced.js
@@ -30,23 +30,43 @@ The number of nodes in the tree is in the range [0, 5000].
 
 // Time Complexity: O(n) where is number of nodes in the tree
 // Space Complexity: O(h) where h is the height of the tree
-function isBalanced(root) {
-    let balanced = true;
-    dfs(root);
-    return balanced;
-    function dfs(root) {
-      // Base Case
-      // If root is null, return 0
-      if (!root) return 0;
-  
-      // Recursive Case
-      // Obtain left height
-      const hL = dfs(root.left);
-      // Obtain right height
-      const hR = dfs(root.right);
-      // Update balanced:
-      if (balanced) balanced = Math.abs(hL - hR)<=1;
-      // return new height
-      return Math.max(hL, hR)+1;
-    }
+function isBalanced1(root) {
+  let balanced = true;
+  dfs(root);
+  return balanced;
+  function dfs(root) {
+    // Base Case
+    // If root is null, return 0
+    if (!root) return 0;
+
+    // Recursive Case
+    // Obtain left height
+    const hL = dfs(root.left);
+    // Obtain right height
+    const hR = dfs(root.right);
+    // Update balanced:
+    if (balanced) balanced = Math.abs(hL - hR) <= 1;
+    // return new height
+    return Math.max(hL, hR) + 1;
   }
+}
+
+//   Similar approach but use array rather closure
+function isBalanced1(root) {
+  return dfs(root)[0];
+  function dfs(root) {
+    // Base Case
+    // If root is null, return true and height of0
+    if (!root) return [true, 0];
+
+    // Recursive Case
+    // Evaluate left node
+    const left = dfs(root.left);
+    // Evaluate right node
+    const right = dfs(root.right);
+    // Update balanced:
+    const balanced = left[0] && right[0] && Math.abs(left[1] - right[1]) <= 1;
+    // return balanced and newhegith
+    return [balanced, Math.max(hL, hR) + 1];
+  }
+}
