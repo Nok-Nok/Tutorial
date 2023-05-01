c5408537e6f541009b72d0ad194a103572d8606f
Nok-Nok <myngoc311@gmail.com>
2023-05-01 10:16:04 -0400

    added another approach for Longest Substring without repeating characters

--- Algorithm/Sliding Window/lengthOfLongestSubstring.js
+++ Algorithm/Sliding Window/lengthOfLongestSubstring.js
@@ -32,7 +32,7 @@ s consists of English letters, digits, symbols and spaces.
 
 // Time complexity: O(n) since we loop through each char of the string
 // Space complexity: O(n) if all characters are unique
-function lengthOfLongestSubstring(s) {
+function lengthOfLongestSubstring1(s) {
   // Intiailize the sliding window
   let iS = 0;
   const pos = {};
@@ -57,5 +57,28 @@ function lengthOfLongestSubstring(s) {
   return Math.max(maxLength, s.length - iS);
 }
 
+// Other approach
+function lengthOfLongestSubstring(s) {
+  // Initialize a set to store character of a substring
+  const charSet = new Set();
+  // Initialize left pointer of the sliding window
+  let l = 0;
+  // Intialize maxLength of substring
+  let maxLength = 0;
+
+  // Loop through the str
+  for (r in s) {
+    // If found duplicate characters, remove char from set until all char are unique
+    while (charSet.has(s[r])) {
+      charSet.delete(s[l++]);
+    }
+    // Add char to charSet
+    charSet.add(s[r]);
+    // Update maxLength
+    maxLength = Math.max(maxLength, r - l + 1);
+  }
+  // Return result
+  return maxLength;
+}
 // s = 'abcbabcbbdcbabbbabchgul';
 // console.log(lengthOfLongestSubstring(s));
