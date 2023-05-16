3c9a7f47b3e19d706ace2834f2144bd19344e326
Nok-Nok <myngoc311@gmail.com>
2023-05-15 23:45:16 -0400

    Complete invert binary tree

---
+++ Algorithm/Tree/invertTree.js
@@ -0,0 +1,37 @@
+// Invert Binary Tree
+
+/**
+ * Given the root of a binary tree, invert the tree, and return its root.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+Example 2:
+
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ */
+
+// Time Complexity: O(n) where n is number of node in the tree
+// Space Complexity: O(h) where h is the height of the tree
+
+function invertTree(root) {
+  if (!root) return root;
+  [root.left, root.right] = [invertTree(root.right), invertTree(root.left)];
+  return root;
+}
