5f830f9e68abd20948aff6e69763a1ffae90b6b3
Nok-Nok <myngoc311@gmail.com>
2023-05-18 02:15:59 -0400

    Complete is valid BST using dfs

---
+++ Algorithm/Tree/isValidBST.js
@@ -0,0 +1,48 @@
+// Validate Binary Search Tree
+
+/**
+ * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left 
+subtree
+ of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+
+Input: root = [2,1,3]
+Output: true
+Example 2:
+
+
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-231 <= Node.val <= 231 - 1
+
+ */
+// Time Complexity: O(n) where n is number of nodes of the tree
+// Space Complexity: O(h) where h is height of the tree, which can be n is we have skewed tree
+function isValidBST(root, min = -Infinity, max = Infinity) {
+  // Base case: If root is null, return true
+  if (!root) return true;
+
+  // Base case: If root val is not between min and max, return false
+  if (root.val <= min || root.val >= max) return false;
+
+  // Recursive case:
+  return (
+    isValidBST(root.left, min, root.val) &&
+    isValidBST(root.right, root.val, max)
+  );
+}
