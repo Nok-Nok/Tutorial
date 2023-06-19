5ea5ab80811233d5dbed3c5ea29850a014cdef84
Nok-Nok <myngoc311@gmail.com>
2023-06-19 16:00:56 -0400

    Complete review maxProfit with cooldown

--- Algorithm/Dynamic Programing/maxProfit.js
+++ Algorithm/Dynamic Programing/maxProfit.js
@@ -87,3 +87,49 @@ function maxProfit(prices) {
 // console.log(maxProfit(prices));
 prices = [2, 1, 4];
 console.log(maxProfit(prices));
+
+// Review:
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+var maxProfit = function (prices) {
+  // initialize dp [BuyMax, SellMax]
+  const dp = new Array(prices.length + 2)
+    .fill(0)
+    .map((e) => new Array(2).fill(0));
+
+  for (let i = prices.length - 1; i >= 0; i--) {
+    // Option 1 : Buy or skip
+    const curBuyMax = Math.max(-prices[i] + dp[i + 1][1], dp[i + 1][0]);
+    // Option 2: Sell+cooldown or skip
+    const curSellMax = Math.max(prices[i] + dp[i + 2][0], dp[i + 1][1]);
+    dp[i] = [curBuyMax, curSellMax];
+  }
+  // console.log([dp]);
+  return dp[0][0];
+};
+
+// Optimize for constant space:
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+var maxProfit = function (prices) {
+  // Intialize:
+  let curBuyMax = 0;
+  let curBuyMaxAfterCoolDown = 0;
+  let curSellMax = 0;
+  for (let i = prices.length - 1; i >= 0; i--) {
+    // Option 1 : Buy or skip
+    const prevBuyMax = Math.max(-prices[i] + curSellMax, curBuyMax);
+    // Option 2: Sell+cooldown or skip
+    const prevSellMax = Math.max(
+      prices[i] + curBuyMaxAfterCoolDown,
+      curSellMax
+    );
+    // Reassign:
+    curBuyMaxAfterCoolDown = curBuyMax;
+    curBuyMax = prevBuyMax;
+    curSellMax = prevSellMax;
+  }
+  return curBuyMax;
+};
+prices = [1, 2, 3, 0, 2];
+console.log(maxProfit(prices));
