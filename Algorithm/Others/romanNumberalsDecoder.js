3e520eff3a209a6ac97d8e27cd4a9f9c4dcc0b10
Nok-Nok <myngoc311@gmail.com>
2023-06-21 14:17:13 -0400

    complete roman to number

---
+++ Algorithm/Others/romanNumberalsDecoder.js
@@ -0,0 +1,67 @@
+// Roman Numerals Decoder
+
+/**
+ * Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+Example:
+
+solution('XXI'); // should return 21
+Help:
+
+Symbol    Value
+I          1
+V          5
+X          10
+L          50
+C          100
+D          500
+M          1,000
+ */
+
+function solution(roman) {
+  // complete the solution by transforming the
+  // string roman numeral into an integer
+  const symbols = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+  // Sliding window question
+  let l = roman.length - 1;
+  let cur = symbols[roman[l]];
+  let total = 0;
+  for (let r = roman.length - 2; r >= 0; r--) {
+    // If the value to the left is still increasing, add to tot
+    if (symbols[roman[r]] >= symbols[roman[l]]) {
+      total += cur;
+      l = r;
+      cur = symbols[roman[l]];
+    } else {
+      // Update cur, and decrese r till we get out of this
+      cur -= symbols[roman[r]];
+    }
+  }
+
+  return total + cur;
+}
+
+function solution1(roman) {
+  const symbols = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let number = 0;
+  for (let i = 0; i < roman.length; i++) {
+    if (symbols[roman[i]] < symbols[roman[i + 1]]) {
+      number -= symbols[roman[i]];
+    } else {
+      number += symbols[roman[i]];
+    }
+  }
+  return number;
+}
+roman = 'IX';
+console.log(solution(roman));
