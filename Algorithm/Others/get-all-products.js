1fdb446f74cd7f205e8313bda733a06a5ebd1d19
Nok-Nok <myngoc311@gmail.com>
2023-06-24 02:49:56 -0400

    Added optimal approach for get-all-products

--- Algorithm/Others/get-all-products.js
+++ Algorithm/Others/get-all-products.js
@@ -19,25 +19,46 @@ Be careful in this problem! What if there is a zero (or multiple zeroes) in the
 input array? How would you handle this?
 
 */
-
+// Time Complexity: O(n) where n is length of array
+// Space Complexity: O(2n) where n is length of array
 const getAllProducts = (array) => {
-  // Get the left Product
-  let prev = 1;
-  const leftProduct = [];
-  for (let i = 0; i < array.length; i++) {
-    prev *= array[i - 1] ?? 1;
-    leftProduct[i] = prev;
+  // Initialize a prefix array to store the product of the prefix
+  const prefix = new Array(array.length).fill(1);
+  for (let i = 1; i < array.length; i++) {
+    // Current prefix product = previous prefix product * previous number
+    prefix[i] = prefix[i - 1] * array[i - 1];
   }
-
-  // Get the right Product and mutiply with the left Product to get final ones.
-  prev = 1;
-  const prod = [];
+  // Initialize a suffix array to store the product of the suffix
+  let curSuffix = 1;
+  const suffix = [];
   for (let i = array.length - 1; i >= 0; i--) {
-    prev *= array[i + 1] ?? 1;
-    prod[i] = prev * leftProduct[i];
+    // Current suffix product = previous suffix product * previous number
+    curSuffix *= array[i + 1] ?? 1;
+    suffix[i] = curSuffix;
   }
-  return prod;
+  // Return result array, which is product of prefix * suffix
+  return suffix.map((_, i) => prefix[i] * suffix[i]);
 };
 
-// getAllProducts([1, 7, 3, 4]);
+// Time Complexity: O(n) where n is length of array
+// Space complexity: O(1) if we do not count result array into space complexity
+const getAllProductsOptimize = (array) => {
+  // Initialize a result array to store the product of the prefix * suffix
+  const result = new Array(array.length).fill(1);
+  for (let i = 1; i < array.length; i++) {
+    // Current prefix product = previous prefix product * previous number
+    result[i] = result[i - 1] * array[i - 1];
+  }
+  // Initialize a suffix variable to store the product of the suffix
+  let suffix = 1;
+  for (let i = array.length - 1; i >= 0; i--) {
+    // Current suffix product = previous suffix product * previous number
+    suffix *= array[i + 1] ?? 1;
+    // Update result
+    result[i] *= suffix;
+  }
+  return result;
+};
+
+getAllProducts([0, 1, 2, 3, 4]);
 // getAllProducts([1, 0, 3, 4]);
